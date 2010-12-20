94bb840bee1be761afee476c66abafec0ef35a45
devyn <devyn.cairns@gmail.com>
2010-12-20 13:20:38 -0800

    Ball orbits circularly. Somewhat helpful comments added.

--- src/main/scala/devyn/luta/engine/test/Ball.scala
+++ src/main/scala/devyn/luta/engine/test/Ball.scala
@@ -9,12 +9,19 @@ class Ball extends Component {
   var acc: Int = 0
 
   def update(dt: Int) {
+    // Increment the accumulator.  Possible error when the accumulator
+    // steps over the maximum value for Int.
     acc += dt
+
+    // Use sine and cosine functions to make a perfect circular orbit.
     x = sin(acc.toDouble / 500).toFloat * 100 + 300
-    y = x
+    y = cos(acc.toDouble / 500).toFloat * 100 + 300
   }
 
   def render(g: Graphics) {
+    // Draw  the  circle  anti-aliased.  It is necessary  to  draw the
+    // outline of the circle after filling it; the fill alone does not
+    // get anti-aliased.
     g.setAntiAlias(true)
     g.fillOval(x, y, 50, 50)
     g.drawOval(x, y, 50, 50)
